9a2935ae8bc923a944d135ba338f766544b439f6
Mattia Fantoni <mattia.fantoni@gmail.com>
2023-10-30 19:01:27 +0000

    Python3.11 and botocore optional deps (#8)
    * marked botocore as optional dep
    
    * python3.11
    
    * removed 3.11 from musllinux.yaml, refactor would be required

--- python/orjson_ddb/boto_integration.py
+++ python/orjson_ddb/boto_integration.py
@@ -1,12 +1,16 @@
 import contextlib
 
-from botocore.parsers import PROTOCOL_PARSERS
+try:
+    from botocore.parsers import PROTOCOL_PARSERS
+except ImportError as e:
+    PROTOCOL_PARSERS = None
 from .orjson_ddb import loads
 
 
 @contextlib.contextmanager
 def ddb_json_parser():
-    # Start the timer
+    if PROTOCOL_PARSERS is None:
+        raise RuntimeError("Module 'botocore' needs to be installed")
     _parser = PROTOCOL_PARSERS.get("json")
     old_parser = getattr(_parser, "_handle_json_body")
 
